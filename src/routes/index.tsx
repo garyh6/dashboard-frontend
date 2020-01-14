04d0df6c976a826e625b7afacee13886cf93ef3e
Gary Hsiao <garyh6@users.noreply.github.com>
2020-01-13 18:39:14 -0800

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "../components/Dashboard";
+import Home from "../components/Home";
+
+export default () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/dashboard" exact component={Dashboard} />
+    </Switch>
+  </BrowserRouter>
+);
